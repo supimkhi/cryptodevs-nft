c264234cac232a0c8c030b1a11137538456de8ed
Khiri Jenkins <khirijenkins@Khiris-MacBook-Pro.local>
2022-06-22 17:11:28 -0500

    First commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xDD581382F7f28cd24a8b04584B2a1F7c566BC269";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
---
+++ hardhat-tutorial/contracts/IWhitelist.sol
@@ -0,0 +1,6 @@
+//SPDX-License-Identifier: Mit
+pragma solidity ^0.8.4;
+
+interface IWhitelist {
+    function whitelistedAddresses(address) external view returns (bool);
+}
